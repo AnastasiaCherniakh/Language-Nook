2b5bb5eacead975e88d92b5babd3e2aad31d841b
AnastasiaCherniakh <anastayshablake@gmail.com>
2022-05-26 19:38:31 +0200

    working version

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const res = require('express/lib/response');
+require("dotenv").config();
+
 const app = express();
-
-app.use(express.static('public'))
-
+app.use(express.json());
+app.use(express.static('public'));
 app.route("/").get(function (req, res) {
     res.sendFile(process.cwd() + "/public/views/index.html");
   });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-const port = process.env.PORT || 3000
-const server = app.listen(port,listening)
+
+app.post('/', (req,res)=> {
+  const output=`
+  <p>Новий студент Language Nook</p>
+  <h3>Контакти:</h3>
+  <ul>
+    <li>Ім\'я: ${req.body.name}</li>
+    <li>Телефон: ${req.body.telephone}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>`;
+
+
+let smtpTrans = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.APP_PASS,
+  },
+});
+
+let mailOptions = {
+  from: '"Новий студент" <testlanguagenook@gmail.com>',
+  to: 'laanguagenook@gmail.com',
+  subject: 'Контакти Language Nook студента ',
+  html: output
+};
+
+smtpTrans.sendMail(mailOptions,(error,info)=>{
+  if(error) {
+    console.log(error);
+    res.send(error);
+  }else {
+    console.log('Message sent: %s', info.response);
+    res.send("success");
+  }
+});
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port,listening);
 function listening (){
     console.log(`server running on ${port}`)
   }
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,6 +3,16 @@ const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElements = document.querySelectorAll('.faded');
 
+const form = document.querySelector('#contact-form');
+const name_ = document.getElementById('name');
+const telephone = document.getElementById('telephone');
+const email = document.getElementById('email');
+
+const name_error = document.getElementById('name-error');
+const telephone_error = document.getElementById('telephone-error');
+const email_error = document.getElementById('email-error');
+const submit_error = document.getElementById('submit-error');
+
 btnMenu.addEventListener('click',function(){
     console.log('click menu');
 
@@ -22,3 +32,85 @@ btnMenu.addEventListener('click',function(){
 
     }
 });
+
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  formData = {
+    name: name_.value,
+    email: email.value,
+    telephone:telephone.value
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST','/');
+  xhr.setRequestHeader('content-type','application/json');
+  xhr.onload = function(){
+      console.log(xhr.responseText);
+      if(xhr.responseText == "success") {
+        submit_error.innerHTML = 'Ми отримали ваші дані!';
+        setTimeout(() =>{
+            form.reset();
+            submit_error.innerHTML = '';
+        }, 3000);
+      }
+      else {
+          alert('Something went wrong...');
+      }
+  }
+
+  xhr.send(JSON.stringify(formData));
+
+});
+
+function validateName() {
+    var name_v = document.getElementById('name').value;
+
+    if(name_v.length==0){
+        name_error.innerHTML = 'Обов\'язкове поле';
+        return false;
+    }
+    name_error.innerHTML = ''
+    return true;
+}
+
+function validatePhone() {
+    var telephone_v = document.getElementById('telephone').value;
+    if(telephone_v.length==0){
+        telephone_error.innerHTML = 'Обов\'язкове поле';
+        return false;
+    }
+
+    if(!telephone_v.match(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)){
+        telephone_error.innerHTML = 'Невірний телефон';
+        return false;
+    }
+    telephone_error.innerHTML = ''
+    return true;
+}
+
+function validateEmail() {
+    var email_v = document.getElementById('email').value;
+    if(email_v.length==0){
+        email_error.innerHTML = 'Обов\'язкове поле';
+        return false;
+    }
+
+    if(!email_v.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+        email_error.innerHTML = 'Невірний email';
+        return false;
+    }
+    email_error.innerHTML = ''
+    return true;
+}
+
+
+function ValidateForm() {
+    if(!validateName() || !validatePhone() || !validateEmail()){
+        return false;
+    }
+};
+
+function validPhone (telephone) {
+    const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return re.test(telephone);
+};
